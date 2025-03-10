373757f1c46127960917cf166de4648b8947bd62
Robson Sandro <robsonsandro@hotmail.com>
2025-03-10 15:09:10 -0300

    Att3

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -2,25 +2,46 @@ import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import{format, formatDistanceToNow} from 'date-fns';
+import { useState } from 'react';
 
 export function Post (props){
+    const [comments, setComments]=useState([
+        1,
+        2
+    ]);
     const publishedDateFormatted = format((props.publishedAt), 'dd/MM/yyyy');
     const publishedDateRaltivetoNow = formatDistanceToNow((props.publishedAt), {addSuffix: true});
+    
+
+    function handleCreateNewComment(){
+        event.preventDefault();
+        setComments([...comments, comments.length + 1]);
+        console.log(comments)
+    }
+
     return (
         <article className={styles.post}>
+
+
             <header>
                 <div className={styles.author}>
+
                     <Avatar hasBorder src={props.author.avatarUrl} alt="foto do maluco" />
+
                     <div className={styles.authorInfo}>
                         <strong>{props.author.name}</strong>
                         <span>{props.author.role}</span>
                     </div>
+               
+               
                 </div>
+
                 <time title={publishedDateFormatted}>{publishedDateRaltivetoNow}</time>
             </header>
 
 
             <div className={styles.content}>
+                
                 {props.content.map(line =>{
                     if(line.type==='paragraph'){
                         return <p>{line.content}</p>
@@ -33,7 +54,8 @@ export function Post (props){
             </div>
 
           
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+
                 <strong>Deixe seu comentario</strong>
                 <textarea placeholder='Deixe um Comentario'/>
                 <footer>
@@ -44,9 +66,9 @@ export function Post (props){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment =>{
+                    return <Comment key={comment} />
+                })}
             </div>
         </article>
 
